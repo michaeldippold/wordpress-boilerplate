01d6b6fc004da05c829e63402ebf3696e5396873
unknown <michael.s.dippold@gmail.com>
2015-04-04 22:08:50 -0500

    moved some css out of the theme css file and into sass, added matchheight to javascript file

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,43 @@
 jQuery( document ).ready(function( $ ) {
 
+	// CODE HERE :)
 
+	/**
+	*
+	*	MATCH HEIGHT BELOW	
+	*
+	**/
+
+	(function ( $ ) { //inline jQuery plugin to match height of elements
+	    $.fn.match_height = function(userOptions, callback) {
+	    	var defaults = {
+	    		mode:'maximum'
+	    	};
+	    	var options = $.extend({}, defaults, userOptions);
+			if(options.mode == 'maximum') {
+				var height=0;
+			} else {
+				var height = this.first().height();
+			}
+			this.each(function() {
+				if(options.mode == 'maximum') {
+					if($(this).height() > height) {
+						height = $(this).height();
+					}	
+				} else if(options.mode == 'minimum') {
+					if($(this).height() < height) {
+						height = $(this).height();
+					}
+				}
+			});
+			this.each(function() {
+				$(this).height(height);
+			});
+			if(typeof callback == 'function') {
+				callback.call(this);
+			}
+			return height;
+		};
+	}( jQuery ));
 
 });
