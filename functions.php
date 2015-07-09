3992e5d571eea5244fe65683484d4f2a34db3ff8
Michael Dippold <michael.s.dippold@gmail.com>
2015-07-09 15:52:18 -0500

    mixins, transition all, update custom js, move wp default and unenqueue theme stle.css

--- functions.php
+++ functions.php
@@ -7,10 +7,6 @@
         wp_deregister_script('jquery'); // Deregister WordPress jQuery
         wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array(), '1.9.1'); // Google CDN jQuery
         wp_enqueue_script('jquery'); // Enqueue it!
-        
-        /* Required theme css file */
-        wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-        wp_enqueue_style('style'); // Enqueue it!
 
         /* Custom styles and js */
         wp_register_style('theme-styles', get_template_directory_uri() . '/css/style.css', array(), '1.0', 'all');
--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,55 @@
+/***
+*
+*	Match Height Plugin
+*
+***/
+
+(function ( $ ) { //inline jQuery plugin to match height of elements
+    $.fn.match_height = function(userOptions, callback) {
+    	var defaults = {
+    		mode:'maximum'
+    	};
+    	var options = $.extend({}, defaults, userOptions);
+		if(options.mode == 'maximum') {
+			var height=0;
+		} else {
+			var height = this.first().height();
+		}
+		this.each(function() {
+			if(options.mode == 'maximum') {
+				if($(this).height() > height) {
+					height = $(this).height();
+				}	
+			} else if(options.mode == 'minimum') {
+				if($(this).height() < height) {
+					height = $(this).height();
+				}
+			}
+		});
+		this.each(function() {
+			$(this).height(height);
+		});
+		if(typeof callback == 'function') {
+			callback.call(this);
+		}
+		return height;
+	};
+}( jQuery ));
+
+/***
+*
+*	Custom JS
+*
+***/
+
 jQuery( document ).ready(function( $ ) {
 
-	// CODE HERE :)
+	// other code :)
 
-	/**
-	*
-	*	MATCH HEIGHT BELOW	
-	*
-	**/
+});
 
-	(function ( $ ) { //inline jQuery plugin to match height of elements
-	    $.fn.match_height = function(userOptions, callback) {
-	    	var defaults = {
-	    		mode:'maximum'
-	    	};
-	    	var options = $.extend({}, defaults, userOptions);
-			if(options.mode == 'maximum') {
-				var height=0;
-			} else {
-				var height = this.first().height();
-			}
-			this.each(function() {
-				if(options.mode == 'maximum') {
-					if($(this).height() > height) {
-						height = $(this).height();
-					}	
-				} else if(options.mode == 'minimum') {
-					if($(this).height() < height) {
-						height = $(this).height();
-					}
-				}
-			});
-			this.each(function() {
-				$(this).height(height);
-			});
-			if(typeof callback == 'function') {
-				callback.call(this);
-			}
-			return height;
-		};
-	}( jQuery ));
+$(window).load(function() {
+	
+	// Match heights :)
 
 });
